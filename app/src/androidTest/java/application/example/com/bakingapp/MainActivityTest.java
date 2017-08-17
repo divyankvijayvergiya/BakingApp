096ac26933b81bd9f92f8ad16370de60c800da47
Divyank Vijayvergiya <divyankvijay.96@gmail.com>
2017-08-18 03:34:56 +0530

    espresso testing implemented

--- app/src/androidTest/java/application/example/com/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/application/example/com/bakingapp/MainActivityTest.java
@@ -1,10 +1,12 @@
 package application.example.com.bakingapp;
 
+import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.ViewAssertion;
 import android.support.test.espresso.ViewInteraction;
-import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -23,7 +25,9 @@ import static android.support.test.espresso.contrib.RecyclerViewActions.actionOn
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Created by Dell on 15-08-2017.
@@ -57,7 +61,24 @@ public class MainActivityTest {
     public static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
         return new RecyclerViewMatcher(recyclerViewId);
     }
+    public class RecyclerViewItemCountAssertion implements ViewAssertion {
+        private final int expectedCount;
 
+        public RecyclerViewItemCountAssertion(int expectedCount) {
+            this.expectedCount = expectedCount;
+        }
+
+        @Override
+        public void check(View view, NoMatchingViewException noViewFoundException) {
+            if (noViewFoundException != null) {
+                throw noViewFoundException;
+            }
+
+            RecyclerView recyclerView = (RecyclerView) view;
+            RecyclerView.Adapter adapter = recyclerView.getAdapter();
+            assertThat(adapter.getItemCount(), is(expectedCount));
+        }
+    }
 
     @Test
     public void mainActivityTest() {
@@ -66,7 +87,7 @@ public class MainActivityTest {
         onView(withRecyclerView(R.id.recycler_bake_list)
                 .atPositionOnView(0, R.id.main_name))
                 .check(matches(withText("Nutella Pie")));
-        onView(withId(R.id.recycler_bake_list)).perform(RecyclerViewActions.scrollToPosition(4));
+        onView(withId(R.id.recycler_bake_list)).check(new RecyclerViewItemCountAssertion(4));
 
 
         ViewInteraction recyclerView = onView(
