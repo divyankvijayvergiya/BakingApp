34e183a72e0c69c2fc3db29f4d81a710c76b3a83
Divyank Vijayvergiya <divyankvijay.96@gmail.com>
2017-08-18 01:56:31 +0530

    espresso testing

--- app/src/androidTest/java/application/example/com/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/application/example/com/bakingapp/MainActivityTest.java
@@ -19,12 +19,10 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
-import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.not;
 
 /**
  * Created by Dell on 15-08-2017.
@@ -54,32 +52,26 @@ public class MainActivityTest {
             }
         };
     }
+    public static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
+        return new RecyclerViewMatcher(recyclerViewId);
+    }
 
 
         @Test
         public void mainActivityTest() {
-            onView(withId(R.id.recycler_bake_list))
-                    .check(matches(hasDescendant(withText("Nutella Pie"))));
-            onView(withId(R.id.recycler_bake_list))
-                    .check(matches(hasDescendant(withText("Brownies"))));
-            onView(withId(R.id.recycler_bake_list))
-                    .check(matches(hasDescendant(withText("Yellow Cake"))));
-            onView(withId(R.id.recycler_bake_list))
-                    .check(matches(hasDescendant(withText("Cheesecake"))));
-            onView(withId(R.id.recycler_bake_list))
-                    .check(matches(not(hasDescendant(withText("xxxaaa111")))));
-            onView(withId(R.id.recycler_bake_list))
-                    .check(matches(not(hasDescendant(withText("")))));
-            onView(withId(R.id.recycler_bake_list))
-                    .check(matches(not(hasDescendant(withText(" ")))));
+            
+
+            onView(withRecyclerView(R.id.recycler_bake_list)
+                    .atPositionOnView(0, R.id.main_name))
+                    .check(matches(withText("Nutella Pie")));
+
 
             ViewInteraction recyclerView = onView(
                 allOf(withId(R.id.recycler_bake_list)
                         ,
                         isDisplayed()));
         recyclerView.perform(actionOnItemAtPosition(0, click()));
-            onView(withId(R.id.list_steps))
-                    .check(matches(hasDescendant(withText("Recipe Introduction"))));
+
 
 
 
